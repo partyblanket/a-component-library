4f526672bab8ecc361f5875c3a7e56020f1b2fdc
Jan Jaap Braam <jjbraam@gmail.com>
2019-08-24 08:48:12 +0200

    create input component

--- src/input/input.js
+++ src/input/input.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Input() {
-  return <input type='text'></input>;
+function Input({ type = 'text', defaultValue, placeholder }) {
+  const [value, setValue] = useState(defaultValue);
+  function onChange(e) {
+    console.log('change');
+    
+    setValue(e.target.value);
+  }
+  return (
+    <input
+      type={type}
+      defaultValue={defaultValue}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
 }
 
 export default Input;
